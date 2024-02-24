2c61161f23727372737341e127a558862cac4584
Duha Iqbal <DIqbal0519@gmail.com>
2024-02-23 21:51:41 -0500

    Initial Commit

---
+++ ran/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ran
+//
+//  Created by Duha Iqbal on 2/23/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
